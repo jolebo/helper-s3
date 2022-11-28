a4d348ea5d9d1b8baa84d24e0bbb4ae5dd58c34b
Yaayakk <yayaksabara@gmail.com>
2022-11-28 16:28:34 +0700

    change doc

--- src/S3.php
+++ src/S3.php
@@ -20,6 +20,12 @@ class S3
 		]);
 	}
 
+	/**
+	 *  to create bucket storage
+	 * 
+	 *  @param string $config["bucketName"] nama bucket
+	 **/
+
 	function createBucket($bucketName = ""){
 
 	    $result = $this->s3->createBucket([
@@ -29,17 +35,17 @@ class S3
 	}
 
 	/**
-	 *  save file to object storage
+	 *  Put object to bucket storage
 	 * 
-	 *  ACL : $config["acl"] private|public-read|public-read-write|authenticated-read|aws-exec-read|bucket-owner-read|bucket-owner-full-control // permission
-	 * 	Body : $config["body"] // file yang akan diupload
-	 *  Bucket : $config["bucket"] default $_ENV["S3_BUCKET"] // bucket name
-	 *  Key : $config["file_name"] // nama file di bucket
-	 *  SourceFile : $config["body"] // url path / base64
-	 * 	ContentType : $config["contentType"]
+	 *  @param string $config["acl"] untuk mengatur hak akses file cth: private|public-read|public-read-write|authenticated-read|aws-exec-read|bucket-owner-read|bucket-owner-full-control // permission
+	 *  @param string $config["bucket"] nama bucket,jika tidak diisi maka akan diambil dari default default $_ENV["S3_BUCKET"]
+	 *  @param string $config["file_name"]  nama file di yang akan disimpan
+	 *  @param string $config["body"] bisa diisi url path / base64
+	 * 	@param string $config["contentType"] content type dari file yang di upload
+	 * @return object
 	 **/
 
-	function saveFile($config = []){
+	function putObject($config = []){
 		$opr = $this->s3->putObject([
 			"ACL" => isset($config["acl"]) ? $config["acl"] : "public-read",
 			// "Body" => $config["body"],
@@ -53,13 +59,13 @@ class S3
 	}
 
 	/**
-	 * To delete file from object storage
+	 * To delete object from bucket storage
 	 * 
-	 * Bucket : $config["bucket"] 
-	 * Key : $config["file_name"]
+	 *  @param string $config["bucket"] nama bucket,jika tidak diisi maka akan diambil dari default default $_ENV["S3_BUCKET"]
+	 * @param string $config["file_name"] nama file yang akan dihapus
 	 **/
 
-	function deleteFile($config = []){
+	function deleteObject($config = []){
 		$checkObject = $this->s3->doesObjectExist(isset($config["bucket"]) ? $config["bucket"] : $_ENV["S3_BUCKET"],$config["file_name"]);
 
 		if($checkObject){
@@ -71,11 +77,14 @@ class S3
 	}
 
 	/**
-	 * to get file from object storage
-	 * Bucket : $config["bucket"],
-	 * Key : $config["file_name"]
+	 * To get object from bucket storage
+	 * 
+	 * @param string $config["bucket"] nama bucket,jika tidak diisi maka akan diambil dari default default $_ENV["S3_BUCKET"]
+	 * @param string $config["file_name"] nama file yang akan dihapus
+	 * @return object
 	 **/
-	function getFile($config = []){
+
+	function getObject($config = []){
 		$checkObject = $this->s3->doesObjectExist(isset($config["bucket"]) ? $config["bucket"] : $_ENV["S3_BUCKET"],$config["file_name"]);
 
 		$opr = [
